17e1ebe7bad2e01998cdfc2c6931fb46a710aa29
amritpalg <amritpalg@gmail.com>
2021-07-18 15:17:20 +0530

    Add first get Route

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express=require('express');
 const app=express();
+const mockUserData=[{name:'Mark'},{name:'Jill'}]
+
+app.get("/users", function(req,res){
+    res.json({
+        success:true,
+        message:"success",
+        users:mockUserData  
+    })
+})
 
 app.listen(8000,function(){
 console.log("Server is running");
