15b9e446d74c27a9d8c09e2e71938f6d7e13a8f4
amritpalg <amritpalg@gmail.com>
2021-07-19 19:29:58 +0530

    Post api sample

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express=require('express');
+const bodyParser=require('body-parser');
+
 const app=express();
+app.use(bodyParser.json());
 const mockUserData=[{name:'Mark'},{name:'Jill'}]
+
+app.post("/login",function(req,res){
+
+    const username=req.body.username;
+    const password=req.body.password;
+
+    if(username==="admin" && password==="admin")
+    {
+        res.json({
+            success:true,
+            message:'Password and username match',
+            token:'123456789'
+        })
+    }
+    else
+    {
+        res.json({
+            success:false,
+            message:'Password and username does not match.'
+        })
+    }
+})
 
 app.get("/users", function(req,res){
     res.json({
