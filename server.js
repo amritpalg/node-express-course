174168addb8dd90c4a82316c537e79fd82bfcf6e
amritpalg <amritpalg@gmail.com>
2021-07-19 19:09:52 +0530

    Request Parameter read

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.get("/users", function(req,res){
     })
 })
 
+app.get("/user/:id", function(req,res){
+    console.log(req.params.id);
+    res.json({
+        success:true,
+        message:"success",
+        users:req.params.id
+    })
+})
+
 app.listen(8000,function(){
 console.log("Server is running");
 });
